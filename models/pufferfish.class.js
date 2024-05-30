62bb3fb9ecca8d2b1291832d8b413a338e58e97d
Christian Frei <christian.frei.01@googlemail.com>
2024-05-30 08:18:05 +0200

    Adjust puffer fish spawning area

--- models/pufferfish.class.js
+++ models/pufferfish.class.js
@@ -16,8 +16,8 @@ class PufferFish extends MovableObject {
     constructor() {
         super().loadImage('img/2.enemies/1.puffer-fish/1.swim/1.swim1.png');
         this.loadImages(this.IMAGES_SWIM);
-        this.x = 200 + Math.random() * 500;
-        this.y = Math.random() * 420;
+        this.x = 320 + Math.random() * 400;
+        this.y = Math.random() * 320;
         this.speed = 0.15 + Math.random() * 0.5;
         this.animate();
     }
